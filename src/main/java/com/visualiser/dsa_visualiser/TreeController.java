b03c3e25be49e6328be48249b436013dbc6e2c6b
Faisal-9999 <mukhtarfaisal03@gmail.com>
2025-03-10 10:36:55 +0500

    added breadth wise insertion in Tree just need to make it work with JavaFX

--- src/main/java/com/visualiser/dsa_visualiser/TreeController.java
+++ src/main/java/com/visualiser/dsa_visualiser/TreeController.java
@@ -3,12 +3,15 @@ package com.visualiser.dsa_visualiser;
 import com.visualiser.miscellaneous.SceneSwitcher;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 //TODO: ADD GUI ELEMENTS TO tree.fxml and connect with this backend class also add the BFS search below
 //TODO: FIND A WAY TO DISPLAY THE NODES PROPERLY WITH NO COLLISIONS
@@ -34,24 +37,73 @@ class Tree<T> {
         return depth;
     }
 
+    public void incrementDepth() {
+        depth++;
+    }
+
     public void addNode(T data) {
         if (root == null) {
             root = new Node<>(data);
             return;
         }
 
-        //TODO: ADD BREADTH FIRST SEARCH LOGIC TO ADD NODE BREADTH WISE
+        LinkedList<Node> q = new LinkedList<>();
+
+        q.add(root);
+
+        while (!q.isEmpty()) {
+
+            Node<T> current =  q.poll();
+
+            if (current.left == null) {
+                current.left = new Node<>(data);
+                break;
+            } else if (current.right == null) {
+                current.right = new Node<>(data);
+                break;
+            }
+            else {
+                q.add(current.left);
+                q.add(current.right);
+            }
+        }
+    }
+
+    public void level_traversal() {
+        if (root == null) {
+            return;
+        }
+
+        ArrayList<ArrayList<T>> nodes = new ArrayList<>();
+
+        LinkedList<Node<T>> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+
+            ArrayList<T> list = new ArrayList<>();
+
+            int length = q.size();
+
+            for (int i = 0; i < length; i++) {
+                if (q.peekFirst().left != null) q.add(q.peekFirst().left);
+                if (q.peekFirst().right != null) q.add(q.peekFirst().right);
+                list.add(q.poll().data);
+            }
+
+            nodes.add(list);
+        }
     }
 
     public void deleteNode(int data) {
 
     }
+
 }
 
 public class TreeController {
 
-    private int NODE_RADIUS = 50;
-    private int currentNodes = 0;
+    private final int NODE_RADIUS = 50;
 
     private Stage stage;
 
@@ -62,6 +114,9 @@ public class TreeController {
 
     @FXML
     AnchorPane Tree_screen;
+
+    @FXML
+    TextField delete_field, add_field;
 
     @FXML
     private void onAddClick() {
